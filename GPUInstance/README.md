eeb5cdb1693926ea7d5b2d0f32d206a5a31b3fcf
Xue <setsuodu@users.noreply.github.com>
2025-11-14 13:36:43 +0800

    create projects

---
+++ GPUInstance/README.md
@@ -0,0 +1,13 @@
+# Intruduce
+
+http://s0.v100.vip:31059/archives/gpu-instancing
+
+# GOAL
+
+- URP Support (built-in support is better);
+- Not only humaniod, but also birds, fish, animals, aliens;
+- Baker texture and script control instance behaviour;
+
+# Grass
+
+//TODO:
---
+++ SourceGen/Generator/Generator/GetComponentGenerator.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Text;
+
+[Generator(LanguageNames.CSharp)]
+public class GetComponentGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        // 我们不依赖任何语法，直接无条件生成代码
+        var source = SourceText.From(GenerateSource(), Encoding.UTF8);
+        context.RegisterPostInitializationOutput(ctx =>
+        {
+            ctx.AddSource("ComponentExtensions_Get.g.cs", source);
+        });
+    }
+
+    private static string GenerateSource()
+    {
+        return @"
+using UnityEngine;
+
+namespace UnityEngine
+{
+    public static partial class ComponentExtensions
+    {
+        /// <summary>
+        /// 手写 SourceGen 版本的 GetComponent（不依赖任何特性糖衣）
+        /// </summary>
+        public static T Get<T>(this Component self) where T : Component
+        {
+            return self.GetComponent<T>();
+        }
+
+        public static T Get<T>(this GameObject self) where T : Component
+        {
+            return self.GetComponent<T>();
+        }
+
+        public static T GetInChildren<T>(this Component self, bool includeInactive = false) where T : Component
+        {
+            return self.GetComponentInChildren<T>(includeInactive);
+        }
+
+        public static T GetInChildren<T>(this GameObject self, bool includeInactive = false) where T : Component
+        {
+            return self.GetComponentInChildren<T>(includeInactive);
+        }
+
+        public static T GetInParent<T>(this Component self, bool includeInactive = false) where T : Component
+        {
+            return self.GetComponentInParent<T>(includeInactive);
+        }
+    }
+}
+";
+    }
+}
